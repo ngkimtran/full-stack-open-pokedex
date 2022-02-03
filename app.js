d7bd2f1886ef0ab7b89fd86ed39828badb6dcbc4
Kim Tran <ngockimtranle@gmail.com>
2022-02-03 16:14:17 +0200

    Update package.json

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express();
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000;
-const HOST = '0.0.0.0';
 
 app.use(express.static('dist'));
 
@@ -15,6 +14,6 @@ app.get('/version', (req, res) => {
   res.send('1');
 });
 
-app.listen(PORT, HOST, () => {
+app.listen(PORT, () => {
   console.log('server started on port 5000'); // eslint-disable-line no-console
 });
